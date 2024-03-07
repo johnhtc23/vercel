afe0fd541fa9638a98973a4dc0754bf2baeca684
John Alexander <118060334+johnhtc23@users.noreply.github.com>
2024-03-07 19:52:22 +0530

    Update aws.ts

--- vercel-upload-service/src/aws.ts
+++ vercel-upload-service/src/aws.ts
@@ -2,9 +2,9 @@ import { S3 } from "aws-sdk";
 import fs from "fs";
 
 const s3 = new S3({
-    accessKeyId: "7ea9c3f8c7f0f26f0d21c5ce99d1ad6a",
-    secretAccessKey: "b4df203781dd711223ce931a2d7ca269cdbf81bb530de4548474584951b798be",
-    endpoint: "https://e21220f4758c0870ba9c388712d42ef2.r2.cloudflarestorage.com"
+    accessKeyId: "you need to put your own accesskeyid from cloudflare r2 or aws s2",
+    secretAccessKey: "you need to put your own secretAccessKey from cloudflare r2 or aws s2",
+    endpoint: "you need to put your own endpoint from cloudflare r2 or aws s2"
 })
 
 // fileName => output/12312/src/App.jsx
